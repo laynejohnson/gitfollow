4045df7b8171bc90f0b2d32c625440f3d25bde84
Layne Johnson <layne@findum.com>
2022-03-21 12:54:46 -0400

    Build search vc, add Constants file

---
+++ GitFollow/App/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  GitFollow
+//
+//  Created by Layne Johnson on 3/21/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Images {
+        static let logo = "gh-logo"
+        static let avatarPlaceholder = "avatar-placeholder"
+        static let emptyLogo = "empty-state-logo"
+    }
+}
--- GitFollow/Controllers/SearchVC.swift
+++ GitFollow/Controllers/SearchVC.swift
@@ -8,9 +8,68 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        // Add logo image subview to view.
+        view.addSubview(logoImageView)
+        
+        // Use auto layout.
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Set image.
+        logoImageView.image = UIImage(named: Constants.Images.logo)!
+        
+        // Activate array of layout constraints.
+        NSLayoutConstraint.activate([
+            // Y coordinate.
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            // X coordinate.
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // Set height.
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            // Set width.
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
