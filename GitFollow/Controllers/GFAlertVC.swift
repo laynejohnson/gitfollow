959b38ff5d2cd0e28bf53252935246b1f95dc9d3
Layne Johnson <layne@findum.com>
2022-03-24 09:29:18 -0400

    Create custom alert vc, configure container view

---
+++ GitFollow/Controllers/GFAlertVC.swift
@@ -0,0 +1,46 @@
+//
+//  GFAlertVC.swift
+//  GitFollow
+//
+//  Created by Layne Johnson on 3/24/22.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+
+    let containerView = UIView()
+    // TODO: Create custom GFAlertContainer.
+    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlignment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok 👍")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    init(title:String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+    }
+    
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
